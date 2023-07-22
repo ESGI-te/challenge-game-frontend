0b4f42fce60df90989a5d752a4b36e6d23f8d26d
tlugat <teo.lugat@ausha.co>
2023-07-22 18:05:31 +0200

    ✨ Friends ws

---
+++ src/websockets/lobby.ws.js
@@ -0,0 +1,48 @@
+import { reactive } from 'vue'
+import { io } from 'socket.io-client'
+import { WS_LOBBY_NAMESPACE } from '@/utils/constants'
+
+export const state = reactive({
+  connected: false,
+  notification: null,
+  lobby: null,
+  error: null
+})
+
+const URL = import.meta.env.VITE_WS_URL + WS_LOBBY_NAMESPACE
+
+const socket = io(URL, {
+  // reconnectionDelayMax: 10000,
+  autoConnect: false,
+  auth: {
+    token: localStorage.getItem('token')
+  }
+})
+
+socket.on('connect', () => {
+  state.connected = true
+})
+
+socket.on('disconnect', () => {
+  state.connected = false
+})
+
+socket.on('lobby', (data) => {
+  state.lobby = data
+})
+
+socket.on('connect_error', (err) => {
+  console.error(err.message)
+  state.error = err.message
+})
+
+socket.on('error', (err) => {
+  state.error = err
+  console.log(err)
+})
+
+socket.on('notification', (msg) => {
+  state.notification = msg
+})
+
+export default socket
