8224098e8692f81b59fb4aa4da49782600471a57
Chakib <karmimchakib@gmail.com>
2023-07-23 19:17:16 +0200

    stripe service link with front-end

--- src/components/StripeCheckoutButton/StripeCheckoutButton.vue
+++ src/components/StripeCheckoutButton/StripeCheckoutButton.vue
@@ -1,35 +1,43 @@
 <script setup>
-import { useCreateStripeCheckoutSessionMutation } from 'queries/stripeCheckoutSession/useCreateStripeCheckoutSessionMutation';
-import { ref } from 'vue';
-import stripe from 'utils/stripe';
+import { useCreateStripeCheckoutSessionMutation } from 'queries/stripeCheckoutSession/useCreateStripeCheckoutSessionMutation'
+import { ref } from 'vue'
+import stripe from 'utils/stripe'
 
-const SUCCESS_URL = 'http://localhost:3000/success';
-const CANCEL_URL = 'http://localhost:3000/cancel';
+const SUCCESS_URL = 'http://localhost:3000/success'
+const CANCEL_URL = 'http://localhost:3000/cancel'
 
-const isLoading = ref(false);
-const createCheckoutSession = useCreateStripeCheckoutSessionMutation();
+const isLoading = ref(false)
+const createCheckoutSession = useCreateStripeCheckoutSessionMutation()
+
+const props = defineProps({
+    name : String,
+    description : String,
+    price : Number,
+    quantity : Number
+});
+
 
 const handlePayment = () => {
-    const products = [
-        'test-product-id'
-    ];
-    isLoading.value = true;
-    createCheckoutSession.mutate(products, {
-        onSuccess: (session) => {
-            stripe.redirectToCheckout({ sessionId: session.id });
-            isLoading.value = false;
-        },
-    });
-};
-
+  const product = {
+    "name": props.name,
+    "description": props.description,
+    "price": props.price,
+    "quantity": props.quantity,
+    "success_url": SUCCESS_URL,
+    "cancel_url": CANCEL_URL
+  }
+  isLoading.value = true
+  createCheckoutSession.mutate(product, {
+    onSuccess: (session) => {
+      stripe.redirectToCheckout({ sessionId: session.id })
+      isLoading.value = false
+    }
+  })
+}
 </script>
 
 <template>
-    <button @click="handlePayment">
-        Pay
-    </button>
+  <button @click="handlePayment">Pay</button>
 </template>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
