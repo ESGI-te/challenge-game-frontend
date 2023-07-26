64296e9a2b9291b37823535d48a6aad267ef61b5
tlugat <teo.lugat@ausha.co>
2023-07-26 18:27:33 +0200

    fix quizz creation

--- src/components/admin/QuizzForm/QuizzForm.schema.js
+++ src/components/admin/QuizzForm/QuizzForm.schema.js
@@ -4,7 +4,7 @@ import * as yup from 'yup'
 const schema = yup.object().shape({
   name: yup.string().min(2).max(200).required(FORM_VALIDATION_MESSAGES.required),
   slogan: yup.string().min(15).max(200).notRequired(),
-  quizz: yup.array().min(1).max(80).required(FORM_VALIDATION_MESSAGES.required),
+  questions: yup.array().min(1).max(80).required(FORM_VALIDATION_MESSAGES.required),
   difficulty: yup.number().min(1).max(5).required(FORM_VALIDATION_MESSAGES.required),
   theme: yup.string().min(1).max(200).required(FORM_VALIDATION_MESSAGES.required)
 })
