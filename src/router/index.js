eee92a286017c3407fba5af18992412997bb30fe
tlugat <teo.lugat@ausha.co>
2023-07-26 19:18:42 +0200

    ✨ Admin route guard

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 import { useAuthStore } from '@/stores/auth.store'
 import { storeToRefs } from 'pinia'
+import { USER_ROLES } from '@/utils/constants'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,16 @@ const router = createRouter({
 
 /* Navigation Guard */
 router.beforeEach(async (to, from) => {
-  const { isAuthenticated, returnUrl } = storeToRefs(useAuthStore())
+  const { isAuthenticated, returnUrl, userRoles } = storeToRefs(useAuthStore())
   const publicRoutes = ['login', 'register']
+  const isAdminRoute = to.path.startsWith('/admin')
 
   if (!isAuthenticated.value && !publicRoutes.includes(to.name)) {
     returnUrl.value = to.fullPath
     return { name: 'login' }
+  }
+  if (isAdminRoute && !userRoles.value.includes(USER_ROLES.ADMIN)) {
+    return from
   }
 })
 
