93ebb4443ec61f36bafcae1a3eddefb6b3e2408d
Chakib <karmimchakib@gmail.com>
2023-07-27 20:58:59 +0200

    💄 friend list style

--- src/components/UserFriendsList/UserFriendsList.vue
+++ src/components/UserFriendsList/UserFriendsList.vue
@@ -1,19 +1,35 @@
 <script setup>
-import { useUserFriendsQuery } from 'queries/userFriends/useUserFriendsQuery';
+import { useUserFriendsQuery } from 'queries/userFriends/useUserFriendsQuery'
+import UserFriendCard from '@/components/UserFriendCard'
+import styled from 'vue3-styled-components'
+import { state } from '@/websockets/friends.ws'
 
-const { data: friends } = useUserFriendsQuery();
+const { data: friends } = useUserFriendsQuery()
 
+console.log(state.offline)
+
+const isConnected = (id) => {
+  const isOnline = state.online.some((friend) => friend.id === id)
+  return isOnline
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`
 </script>
 
 <template lang="">
-    <h2>My friends</h2>
-    <div>
-        <div v-for="friend in friends" :key="friend._id">
-            <p>{{ friend.username }}</p>
-        </div>
-    </div>
+  <Wrapper>
+    <UserFriendCard
+      v-for="friend in friends"
+      :key="friend._id"
+      :username="friend.username"
+      :logged="isConnected(friend._id)"
+    />
+  </Wrapper>
 </template>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
