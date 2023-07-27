b5e599431df9346cc5548bcc3243dd54ef40ea5c
Chakib <karmimchakib@gmail.com>
2023-07-27 22:24:00 +0200

    💄 friend invitation and request style

--- src/components/UserInvitationForm/UserInvitationForm.vue
+++ src/components/UserInvitationForm/UserInvitationForm.vue
@@ -1,27 +1,47 @@
 <script setup>
-import { ref } from 'vue';
+import InputText from '@/components/InputText'
+import Button from '@/components/Button'
+import { ref } from 'vue'
+import { useInviteUserMutation } from 'queries/userInvitation/useInviteUserMutation'
+import styled from 'vue3-styled-components'
 
-const username = ref('');
-import { useInviteUserMutation } from 'queries/userInvitation/useInviteUserMutation';
-const inviteUser = useInviteUserMutation();
+const username = ref('')
+const inviteUser = useInviteUserMutation()
 
 const sendInvitation = () => {
-    inviteUser.mutate(username.value, {
-        onSuccess: () => {
-            username.value = '';
-        }
-    });
+  inviteUser.mutate(username.value, {
+    onSuccess: () => {
+      username.value = ''
+    }
+  })
 }
 
+const AddInput = styled(InputText)`
+  ${({ theme }) => theme.mediaQueries.desktopAndUp} {
+    max-width: 25rem;
+  }
+`
+
+const AddForm = styled.form`
+    width:100%;
+    display: flex;
+    flex-direction: column;
+    row-gap : 1rem;
+`
+
+const AddButton = styled(Button)`
+  width : 100%;
+  ${({ theme }) => theme.mediaQueries.desktopAndUp} {
+    max-width: 25rem;
+  }
+`
 </script>
 
 <template>
-    <form @submit.prevent="sendInvitation">
-        <input :disabled="isLoading" v-model="username" type="text" />
-        <button :disabled="isLoading" type="submit">Search</button>
-    </form>
+  <AddForm @submit.prevent="sendInvitation">
+    <AddInput :disabled="isLoading" v-model="username" placeholder="username" type="text" />
+    <AddButton :disabled="isLoading" type="submit">Search</AddButton>
+  </AddForm>
 </template>
 
-<style lang="">
-    
-</style>
+<style lang=""></style>
