2105a40173168dc9349d50760075941efdc5d80a
tlugat <teo.lugat@ausha.co>
2023-07-27 11:32:28 +0200

    💄 Dynamic layouts + basic UI components + landing and login styles

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,32 +1,39 @@
 <script setup>
 import styled from 'vue3-styled-components'
-import Logo from '../components/Logo'
+import logo from 'public/img/landing-logo.svg'
 
-const wrapper = styled.div`
+const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   align-content: center;
   gap: 40px;
+  padding: 1rem;
+`
+
+const Logo = styled.img`
+  width: 100%;
+  max-width: 664px;
 `
 </script>
 
 <template>
-  <wrapper>
-    <Logo />
+  <Container>
+    <Logo :src="logo" alt="logo" />
     <h4>Les quizz qui animeront vos soirées</h4>
 
     <RouterLink :to="'/login'">
       <button class="primary bold">Jouer</button>
     </RouterLink>
-  </wrapper>
+  </Container>
 </template>
 
 <style scoped>
 .primary {
   min-width: 342px;
 }
+
 @media screen and (max-width: 630px) {
   h2 {
     max-width: 342px;
