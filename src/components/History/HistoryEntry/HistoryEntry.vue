c5a650cf9a66de8a4877a2bba9b0635aa9b72804
Chakib <karmimchakib@gmail.com>
2023-07-27 15:29:57 +0200

    💄 History list and entry components

--- src/components/History/HistoryEntry/HistoryEntry.vue
+++ src/components/History/HistoryEntry/HistoryEntry.vue
@@ -1,25 +1,67 @@
 <script setup>
+import Text from '@/components/Text'
+import styled from 'vue3-styled-components'
+import gameIcone from 'public/img/game-icone.svg'
+
 const props = defineProps({
-  game: Object
+  data: Object
 })
+
+const formatDate = (dateString) => {
+  const dateObj = new Date(dateString)
+  const month = (dateObj.getUTCMonth() + 1).toString().padStart(2, '0')
+  const day = dateObj.getUTCDate().toString().padStart(2, '0')
+  const hour = dateObj.getUTCHours().toString().padStart(2, '0')
+  const minute = dateObj.getUTCMinutes().toString().padStart(2, '0')
+  return `${month}/${day} - ${hour}h:${minute}`
+}
+
+const card = styled.div`
+  height: 6.5rem;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 1rem 1.5rem;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1.5rem;
+  border: 2px solid #000;
+  background: var(--white);
+`
+const game = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  ${({ theme }) => theme.mediaQueries.desktopAndUp} {
+    height: 5rem;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+
+const GameIcon = styled.img`
+  padding: 0.375rem;
+  width: 3rem;
+  height: 3rem;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #000;
+  background: var(--black, #111);
+`
 </script>
 
 <template>
-  <div>
-    <div class="card">
-      <p>Rank : {{ game.rank }}</p>
-      <p>Score : {{ game.score }}</p>
-      <p>Lives remained : {{ game.lives }}</p>
-      <!-- <small>{{ game.createdAt }}</small> -->
-    </div>
-  </div>
+  <card class="card">
+    <GameIcon :src="gameIcone" />
+    <game>
+      <Text variant="h4"> Game : {{ formatDate(data.createdAt) }} </Text>
+      <Text variant="p"> Score : {{ data.score }}</Text>
+    </game>
+  </card>
 </template>
 
-<style scoped>
-.card {
-    border : 1px black solid;
-    padding : 10px;
-    margin : 10px;    
-    width: 200px;
-}
-</style>
+<style scoped></style>
